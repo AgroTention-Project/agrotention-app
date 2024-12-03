278fa66d9563b6175667ed267d217978235685aa
bhki29 <ajiahmadbaehaki20@gmail.com>
2024-12-04 04:34:31 +0700

    create adapter for news

--- app/src/main/java/com/bangkit/agrotentionapp/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/bangkit/agrotentionapp/data/remote/retrofit/ApiService.kt
@@ -1,4 +1,9 @@
 package com.bangkit.agrotentionapp.data.remote.retrofit
 
+import com.bangkit.agrotentionapp.data.remote.response.NewsResponse
+import retrofit2.http.GET
+
 interface ApiService {
+    @GET("news")
+    suspend fun getNews(): NewsResponse
 }
