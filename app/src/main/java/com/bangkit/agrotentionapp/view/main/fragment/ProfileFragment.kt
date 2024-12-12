af75aa905ddf972fb18f9a678968e3727d7e4c0f
galangriarulhaq <galangrz337@gmail.com>
2024-12-12 09:11:27 +0700

    Fix Bug on the Profile Fragment

--- app/src/main/java/com/bangkit/agrotentionapp/view/main/fragment/ProfileFragment.kt
+++ app/src/main/java/com/bangkit/agrotentionapp/view/main/fragment/ProfileFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.bangkit.agrotentionapp.R
 import com.bangkit.agrotentionapp.databinding.FragmentProfileBinding
+import com.bangkit.agrotentionapp.databinding.FragmentScanBinding
 import com.bangkit.agrotentionapp.view.main.model.ProfileViewModel
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
@@ -15,37 +16,42 @@ import com.google.firebase.auth.FirebaseAuth
 class ProfileFragment : Fragment() {
 
 
-    companion object {
-        fun newInstance() = ProfileFragment()
-    }
-
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
 
     private val viewModel: ProfileViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         val user = FirebaseAuth.getInstance().currentUser
 
         val name = user?.displayName
         val email = user?.email
         val photoUrl = user?.photoUrl
-
-
+        val phoneNumber = user?.phoneNumber ?: "-"
+        
         binding.nameProfile.text = name
         binding.emailProfile.text = email
+        binding.tlpProfile.text = phoneNumber
+
         Glide.with(binding.root)
             .load(photoUrl)
             .into(binding.imageView)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null // Menghindari memory leak
+    }
+
 }
